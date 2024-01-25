e98e1d6af014f76d47d15cac2a68de53064a1d0c
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2024-01-25 10:45:13 +0530

    Add loding effect and correction in code

--- src/components/CountryLookup.jsx
+++ src/components/CountryLookup.jsx
@@ -3,13 +3,13 @@
 import { useEffect, useState } from "react";
 
 export default function CountryLookup() {
-  const [country, setCountry] = useState("United States");
-  useEffect(() => {
-    fetch(
-      `https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((data) => setCountry(data.country));
-  }, []);
+  const [country, setCountry] = useState("India");
+  // useEffect(() => {
+  //   fetch(
+  //     `https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setCountry(data.country));
+  // }, []);
   return <div>{country}</div>;
 }
